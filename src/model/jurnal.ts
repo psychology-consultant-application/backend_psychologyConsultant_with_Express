f381f079309ecadda478121be322d352a0281802
JahfalRzq <126737235+JahfalRzq@users.noreply.github.com>
2025-04-05 20:01:31 +0700

    FEAT : MODEL JURNAL

---
+++ src/model/jurnal.ts
@@ -0,0 +1,72 @@
+import { IsDate, IsInt, IsString,IsUppercase } from "class-validator";
+import { Entity,PrimaryGeneratedColumn,Column,CreateDateColumn,UpdateDateColumn,DeleteDateColumn, OneToMany, ManyToOne, JoinColumn, Int32 } from "typeorm";
+import { User } from "./User";
+
+
+export enum StatusAppoinment {
+    ONGOING = 'ONGOING',
+    COMPLETED = 'DONE'
+}
+
+@Entity()
+export class appoinment{
+
+    @PrimaryGeneratedColumn('uuid')
+    public id : string
+
+    
+    @Column({
+        type: 'varchar', // Tambahkan tipe eksplisit
+        default: null,
+        nullable: true
+    })
+    @IsString()
+    public meetLink: string
+
+    @Column({
+        type: 'int', // Tambahkan tipe eksplisit
+        default: null,
+        nullable: true
+    })
+    @IsInt()
+    public durationMinute: number
+
+    @Column({
+        type: 'timestamp', // Tambahkan tipe eksplisit
+        default: null,
+        nullable: true
+    })
+    @IsDate()
+    public appoinmentDate: Date
+
+
+    @Column({
+        type: 'enum',
+        enum: StatusAppoinment,
+        default: StatusAppoinment.ONGOING
+    })
+    @IsString()
+    @IsUppercase()
+    public status: StatusAppoinment
+
+    @CreateDateColumn()
+    public createdAt: Date
+
+    @UpdateDateColumn()
+    public updatedAt: Date
+
+    @DeleteDateColumn()
+    public deletedAt: Date
+
+
+    @ManyToOne (() => User, (pasienId) => pasienId.appoinmentPasien)
+    @JoinColumn()
+    public pasienId : User
+
+
+    @ManyToOne (() => User, (psychologyId) => psychologyId.appoinmentPsychology)
+    @JoinColumn()
+    public psychologyId : User
+
+
+}
